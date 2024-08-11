eb28ffa7200d6525180f63458eba537821a70423
Gustavo Lima <gu_carvalho22@hotmail.com>
2024-08-11 17:06:09 -0300

    Implementação das configurações do docker + ajuste na _ViewImports.cshtml

--- eCommerceMM/src/ApiGateways/Bff.Compras/Configuration/MessageBusConfig.cs
+++ eCommerceMM/src/ApiGateways/Bff.Compras/Configuration/MessageBusConfig.cs
@@ -8,6 +8,5 @@ public static class MessageBusConfig
     public static void AddMessageBusConfiguration(this IServiceCollection services,
         IConfiguration configuration)
     {
-        services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
     }
 }
